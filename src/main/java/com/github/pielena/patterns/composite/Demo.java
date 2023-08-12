e37c4da401df5055dda7a1c32baf1289995cfeb2
Elena Pilyugina <elena.pilyugina.job@gmail.com>
2023-08-12 17:02:20 +0300

    add empty lines

--- src/main/java/com/github/pielena/patterns/composite/Demo.java
+++ src/main/java/com/github/pielena/patterns/composite/Demo.java
@@ -6,10 +6,12 @@ public class Demo {
         Directory documents = new Directory("documents");
         Directory images = new Directory("images");
         Directory texts = new Directory("texts");
+
         File presentation1 = new File("presentation1.pdf");
         File image1 = new File("image1.jpg");
         File image2 = new File("image1.jpg");
         File text1 = new File("text1.txt");
+
         documents.add(presentation1);
         documents.add(images);
         documents.add(texts);
